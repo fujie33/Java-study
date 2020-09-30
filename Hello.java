7fe3498f7f7c91bc418056605be7a1deab00465f
fujie33 <71702859+fujie33@users.noreply.github.com>
2020-09-30 20:55:44 +0800

    9.30提交

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package com.fujie.study;
+
+public class Hello {
+    public static void main(String[] args) {
+        int sum = 0,i = 0,j = 0;
+        for( i =1;i <= 100;i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
